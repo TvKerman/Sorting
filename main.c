01098308eb4fdec8de263c2c6b73a0a0a60ebed2
Kerman <grigorij.perfilin@mail.ru>
2022-03-13 14:54:45 +0300

    testing sorting functions by the number of comparison operations

--- main.c
+++ main.c
@@ -31,6 +31,13 @@ typedef struct SortFunc {
     char name[64]; // имя сортировки, используемое при выводе
 }SortFunc;
 
+// функция сортировки
+typedef struct SortFuncNComps {
+    long long (*sort)(int *a, size_t n); // указатель на функцию сортировки
+    char name[64]; // имя сортировки, используемое при выводе
+}SortFuncNComps;
+
+
 // функция генерации
 typedef struct GeneratingFunc {
     void (*generate) (int *a, size_t n); // указатель на функцию генерации последоват.
@@ -114,9 +121,9 @@ void timeExperiment () {
     // запись статистики в файл
     for (size_t size = 10000; size <= 100000; size += 10000) {
         printf (" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
-        printf (" Size : %d\n", size );
-        for (int i = 0; i < FUNCS_N ; i ++) {
-            for (int j = 0; j < CASES_N ; j ++) {
+        printf (" Size : %d\n", size);
+        for (int i = 0; i < FUNCS_N ; i++) {
+            for (int j = 0; j < CASES_N ; j++) {
                 // генерация имени файла
                 char filename[128];
                 sprintf(filename, "%s_%s_time", sorts[i].name, generatingFuncs[j].name);
@@ -127,8 +134,87 @@ void timeExperiment () {
     }
 }
 
+void checkNComps(long long (*sortFunc)(int *, size_t), void (*generateFunc)(int *, size_t), size_t size, char *experimentName) {
+    static size_t runCounter = 1;
+
+    // генерация последовательности
+    static int innerBuffer[100000];
+    generateFunc(innerBuffer, size);
+    printf("Run #%zu| ", runCounter++);
+    printf("Name: %s\n", experimentName);
+
+    long long nComps = sortFunc(innerBuffer, size);
+
+    // результаты замера
+    printf (" Status : ") ;
+    if (isOrdered(innerBuffer, size)) {
+        printf ("OK! Count of comparisons: %lld.\n", nComps);
+
+        // запись в файл
+        char filename [256];
+        sprintf(filename, "./data/%s.csv", experimentName);
+        FILE *f = fopen(filename, "a");
+        if (f == NULL) {
+            printf (" FileOpenError %s", filename);
+            exit(1);
+        }
+        fprintf(f, "%zu; %lld\n", size, nComps);
+        fclose(f);
+    } else {
+        printf(" Wrong !\n");
+
+        // вывод массива, который не смог быть отсортирован
+        outputArray_(innerBuffer, size);
+
+        exit(1);
+    }
+}
+
+void comparisonsExperiment() {
+    // описание функций сортировки
+    SortFuncNComps sorts [] = {
+            {getExchangeSortNComp, "exchangeSort"},
+            {getSelectionSortNComp, "selectionSort"},
+            {getInsertionSortNComp, "insertionSort"},
+            {getCombSortNComp, "combSort"},
+            {getShellSortNComp, "shellSort"},
+            {getDigitalSortNComp, "digitalSort"}
+            //{ selectionSort , " selectionSort "} ,
+            //{ insertionSort , " insertionSort "} ,
+            // вы добавите свои сортировки
+    };
+    const unsigned FUNCS_N = ARRAY_SIZE(sorts);
+
+    // описание функций генерации
+    GeneratingFunc generatingFuncs [] = {
+            // генерируется случайный массив
+            {generateRandomArray, "random"},
+            // генерируется массив 0, 1, 2, ..., n - 1
+            {generateOrderedArray, "ordered"},
+            // генерируется массив n - 1, n - 2, ..., 0
+            {generateOrderedBackwards, "orderedBackwards"}
+    };
+    const unsigned CASES_N = ARRAY_SIZE(generatingFuncs);
+
+    // запись статистики в файл
+    for (size_t size = 10000; size <= 100000; size += 10000) {
+        printf (" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+        printf (" Size : %d\n", size);
+        for (int i = 0; i < FUNCS_N ; i++) {
+            for (int j = 0; j < CASES_N; j++) {
+                // генерация имени файла
+                char filename[128];
+                sprintf(filename, "%s_%s_nComps", sorts[i].name, generatingFuncs[j].name);
+                checkNComps(sorts[i].sort, generatingFuncs[j].generate, size, filename);
+            }
+        }
+        printf ("\n") ;
+    }
+}
+
 int main() {
-    timeExperiment();
+    //timeExperiment();
+    comparisonsExperiment();
 
     //int a[] = {1, 5, 3, 0, 1000, -256, 300, 256, 9, 3, -50, -30, -5};
     //exchangeSorting(a, 6);
